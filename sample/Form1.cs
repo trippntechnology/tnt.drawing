03a867ba861447d4316d0a912284ae170475d31d
Steve <stevetripp@comcast.net>
2020-09-23 08:53:16 -0600

    Initial commit

---
+++ sample/Form1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Win32;
+using System.Windows.Forms;
+using TNT.Drawing;
+using TNT.Utilities;
+
+namespace TNT.Drawing.Sample
+{
+	public partial class Form1 : Form
+	{
+		private ApplicationRegistry applicationRegistery = null;
+		private CanvasPanel _MyControl = null;
+
+		public Form1()
+		{
+			InitializeComponent();
+			applicationRegistery = new ApplicationRegistry(this, Registry.CurrentUser, "Tripp'n Technology", "CenteredDrawing");
+			_MyControl = new CanvasPanel(splitContainer1.Panel1);
+			_MyControl.MouseMove += _MyControl_MouseMove;
+			propertyGrid1.SelectedObject = _MyControl.Canvas;
+		}
+
+		private void _MyControl_MouseMove(object sender, MouseEventArgs e)
+		{
+			toolStripStatusLabel1.Text = $"{e.X}, {e.Y}";
+		}
+
+		private void fitToolStripMenuItem_Click(object sender, System.EventArgs e)
+		{
+			_MyControl.Fit();
+		}
+	}
+}
---
+++ tnt.drawing/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TNT.Drawing")]
+[assembly: AssemblyDescription("TNT Drawing")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Tripp'n Technology, LLC.")]
+[assembly: AssemblyProduct("TNTDrawing")]
+[assembly: AssemblyCopyright("Copyright 2020 Tripp'n Technology, LLC.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ac49554d-bfc4-40ba-8a7b-b8dededb63f5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
